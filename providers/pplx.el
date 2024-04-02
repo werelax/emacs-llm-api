ffe241a22cc32834ff0368021ac61eee752b3143
Elias Alonso <elias@aucta.io>
2024-04-02 20:23:32 +0200

    updated pplx models

--- providers/pplx.el
+++ providers/pplx.el
@@ -5,15 +5,14 @@
    :name "pplx"
    :url "https://api.perplexity.ai/chat/completions"
    :token token
-   :available-models '("codellama-34b-instruct"
-                       "llama-2-70b-chat"
+   :available-models '("sonar-small-chat"
+                       "sonar-small-online"
+                       "sonar-medium-chat"
+                       "sonar-medium-online"
                        "mistral-7b-instruct"
                        "mixtral-8x7b-instruct"
-                       "pplx-7b-chat"
-                       "pplx-70b-chat"
-                       "pplx-7b-online"
-                       "pplx-70b-online")
-   :selected-model "openhermes-2.5-mistral-7b"
+                       "codellama-70b-instruct")
+   :selected-model "sonar-medium-online"
    :system-prompt "You are a sentient superintelligent AI assistant.
  Help the user with precision."
    :params '(:temperature 0.2)))
