57824f4a43fab05abd660f0ea9cd12e2551bab2d
Elias Alonso <elias.alonso@aikit.es>
2026-03-02 15:08:28 +0100

    Fix MiniMax: set available-models slot in get-available-models method

--- providers/minimax.el
+++ providers/minimax.el
@@ -7,13 +7,15 @@
                                  (:copier nil)))
 
 ;; MiniMax doesn't have a /models endpoint, so we return a static list
-(cl-defmethod llm-api--get-available-models ((_platform llm-api--minimax))
+(cl-defmethod llm-api--get-available-models ((platform llm-api--minimax))
   "Return static list of MiniMax models."
-  '("MiniMax-M2.5"
-    "MiniMax-M2.5-highspeed"
-    "MiniMax-M2.1"
-    "MiniMax-M2.1-highspeed"
-    "MiniMax-M2"))
+  (let ((models '("MiniMax-M2.5"
+                  "MiniMax-M2.5-highspeed"
+                  "MiniMax-M2.1"
+                  "MiniMax-M2.1-highspeed"
+                  "MiniMax-M2")))
+    (setf (llm-api--platform-available-models platform) models)
+    models))
 
 (defun llm--create-minimax-platform (token &optional selected-model)
   (llm-api--minimax-create
